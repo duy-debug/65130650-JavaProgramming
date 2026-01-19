be36d59f3efdb4b2998ec26413885773874e0106
Tran Mai Ngoc Duy <tmnduy.it@gmail.com>
2026-01-19 21:59:11 +0700

    example-helloworld

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,15 @@
+// Đây là chú thích một dòng
+
+/*
+ * Đây là chú thích nhiều dòng
+ * Chương trình đầu tiên: Hello World
+ */
+// Lớp chính của chương trình
+public class HelloWorld {
+  // Phương thức main
+  public static void main(String[] args){
+    // Lệnh in ra màn hình
+    System.out.println("Hello World!");
+    System.out.print("Hi There! I'm Tran Mai Ngoc Duy - ID: 65130650");
+  }
+}
